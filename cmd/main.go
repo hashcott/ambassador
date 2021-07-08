a033e7b49e5886139dab2130fce69a4685b1d0ee
Nguyen Duc Hanh <duchanhstyle@gmail.com>
2021-07-08 16:11:50 +0700

    structure of project and containerize your go development environment

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,11 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
---
+++ pkg/repository/mysql.go
@@ -0,0 +1,26 @@
+package repository
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type Config struct {
+	Host     string
+	Port     string
+	Username string
+	Password string
+	DBName   string
+}
+
+func NewMysqlDB(cfg Config) (*gorm.DB, error) {
+	db, err := gorm.Open(mysql.Open(fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", cfg.Username, cfg.Password, cfg.Host, cfg.Port, cfg.Username)), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalf("Could not connect with database:  %s", err.Error())
+	}
+	return db, nil
+}
